bf134bd69b65bae76cee0ff91bb81af6a18cda9c
jjoonior <121348399+jjoonior@users.noreply.github.com>
2023-11-02 20:49:15 +0900

    [level 2] Title: 피로도, Time: 0.32 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.js
@@ -0,0 +1,22 @@
+function solution(k, dungeons) {
+    var answer = -1;
+
+    const path = [];
+
+    (function fn (m){
+        for (let i = 0; i < dungeons.length; i++) {
+            if (!path.includes(i)) {
+                if (m>= dungeons[i][0]) {
+                    path.push(i);
+                    m -= dungeons[i][1];
+                    answer = Math.max(answer, path.length);
+                    fn(m);
+                    m += dungeons[i][1];
+                    path.pop(i);
+                }
+            }
+        }
+    })(k);
+    
+    return answer;
+}

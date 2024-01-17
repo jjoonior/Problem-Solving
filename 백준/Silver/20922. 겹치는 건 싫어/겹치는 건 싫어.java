7727ccd97f7375f222e79d19e1b7ad57ebcf8f61
jjoonior <121348399+jjoonior@users.noreply.github.com>
2024-01-17 13:14:57 +0900

    Refactor: [Silver I] Title: 겹치는 건 싫어

--- 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.java
+++ 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.java
@@ -1,34 +1,44 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+  public static void main(String args[]) throws Exception {
+    BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(in.readLine());
+    int N = Integer.parseInt(st.nextToken());
+    int K = Integer.parseInt(st.nextToken());
+    st = new StringTokenizer(in.readLine());
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(in.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
-		int arr[] = new int[N];
-		st = new StringTokenizer(in.readLine());
-		for (int i = 0; i < arr.length; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		int ans = 0;
-		int start = 0;
-		int end = 0;
-		int cnt[] = new int[100001];
-		while(end < arr.length) {
-			while(end < arr.length && cnt[arr[end]] + 1 <= K) {
-				cnt[arr[end]]++;
-				end++;
-			}
-			int len = end-start;
-			ans = Math.max(ans, len);
-			cnt[arr[start]]--;
-			start++;
-		}
-		System.out.println(ans);
-	}
+    int[] A = new int[N];
+    Map<Integer, Integer> map = new HashMap<>();
+
+    for (int i = 0; i < N; i++) {
+      A[i] = Integer.parseInt(st.nextToken());
+      map.put(A[i], 0);
+    }
+
+    int max = 1;
+    int a = 0;
+    int b = 0;
+    map.put(A[0], 1);
+
+    while (a < N) {
+      if (map.get(A[a]) > K) {
+        map.put(A[b], map.get(A[b]) - 1);
+        b++;
+        continue;
+      }
+
+      max = Math.max(max, a - b + 1);
+      a++;
+      if (a < N) {
+        map.put(A[a], map.get(A[a]) + 1);
+      }
+    }
+
+    System.out.println(max);
+  }
 }

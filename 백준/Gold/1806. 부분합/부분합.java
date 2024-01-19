fd7401d30e21a3ec839ae14de975c146500c0916
jjoonior <121348399+jjoonior@users.noreply.github.com>
2024-01-19 12:31:56 +0900

    [Gold IV] Title: 부분합, Time: 292 ms, Memory: 23852 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -1,43 +1,35 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+  public static void main(String args[]) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int N = Integer.parseInt(st.nextToken());
+    int S = Integer.parseInt(st.nextToken());
 
-        int N = Integer.parseInt(st.nextToken());
-        int S = Integer.parseInt(st.nextToken());
-
-        int[] arr = new int[N];
-
-        st = new StringTokenizer(br.readLine());
-
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-
-        int a = 0;
-        int b = 0;
-        int min = 100000;
-        int sum = 0;
-
-        if (Arrays.stream(arr).sum() < S) {
-            System.out.println(0);
-            return;
-        }
-
-        while (true) {
-            if (sum >= S) {
-                sum -= arr[a++];
-                min = Math.min(min, (b - a + 1));
-            } else if (b == N)
-                break;
-            else
-                sum += arr[b++];
-        }
-
-        System.out.println(min);
+    int[] arr = new int[N + 1];
+    st = new StringTokenizer(br.readLine());
+    for (int i = 1; i <= N; i++) {
+      arr[i] = arr[i - 1] + Integer.parseInt(st.nextToken());
     }
-}
 
+    int min = N + 1;
+    int a = 1;
+    int b = 1;
+    while (a <= N) {
+      if (arr[a] - arr[b - 1] >= S) {
+        if (a == b) {
+          System.out.println(1);
+          return;
+        }
+        min = Math.min(min, a - b + 1);
+        b++;
+      } else {
+        a++;
+      }
+    }
+    System.out.println(min == N + 1 ? 0 : min);
+  }
+}

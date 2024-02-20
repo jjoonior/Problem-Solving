38a3a477f869aeadb86c139c1291bb7dc7e20ffd
jjoonior <121348399+jjoonior@users.noreply.github.com>
2024-02-20 19:53:33 +0900

    [Gold IV] Title: 줄세우기, Time: 124 ms, Memory: 14128 KB -BaekjoonHub

---
+++ 백준/Gold/2631. 줄세우기/줄세우기.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  public static void main(String args[]) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine());
+    int[] arr = new int[N];
+
+    for (int i = 0; i < N; i++) {
+      arr[i] = Integer.parseInt(br.readLine());
+    }
+
+    ArrayList<Integer> arrayList = new ArrayList<>();
+
+    arrayList.add(arr[0]);
+
+    for (int i = 1; i < N; i++) {
+      if (arrayList.get(arrayList.size() - 1) < arr[i]) {
+        arrayList.add(arr[i]);
+      } else {
+        int l = 0;
+        int r = arrayList.size() - 1;
+        while (l < r) {
+          int m = (l + r) / 2;
+
+          if (arrayList.get(m) > arr[i]) {
+            r = m;
+          } else if (arrayList.get(m) == arr[i]) {
+            l = m;
+            break;
+          } else {
+            l = m + 1;
+          }
+        }
+
+        arrayList.set(l, arr[i]);
+      }
+    }
+    System.out.println(N - arrayList.size());
+  }
+}

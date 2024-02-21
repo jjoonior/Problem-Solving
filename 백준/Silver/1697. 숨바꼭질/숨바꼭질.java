43a265d2835fe09ff838a8d81b6dd35c0589fdb9
jjoonior <121348399+jjoonior@users.noreply.github.com>
2024-02-21 09:24:58 +0900

    [Silver I] Title: 숨바꼭질, Time: 164 ms, Memory: 18588 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  public static void main(String args[]) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    int N = Integer.parseInt(st.nextToken());
+    int K = Integer.parseInt(st.nextToken());
+    int[] visited = new int[100001];
+    Queue<Integer> q = new LinkedList<>();
+    q.offer(N);
+    visited[N] = 1;
+
+    while (!q.isEmpty()) {
+      int x = q.poll();
+
+      if (x == K) {
+        break;
+      }
+
+      if (x - 1 >= 0 && visited[x - 1] == 0) {
+        q.offer(x - 1);
+        visited[x - 1] = visited[x] + 1;
+      }
+      if (x + 1 <= 100000 && visited[x + 1] == 0) {
+        q.offer(x + 1);
+        visited[x + 1] = visited[x] + 1;
+      }
+      if (x * 2 <= 100000 && visited[x * 2] == 0) {
+        q.offer(x * 2);
+        visited[x * 2] = visited[x] + 1;
+      }
+    }
+
+    System.out.println(visited[K] - 1);
+  }
+}

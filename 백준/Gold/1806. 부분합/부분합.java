9bff6a67ee4e24cf8f4b563d9fe5e6bea7ea63ca
jjoonior <121348399+jjoonior@users.noreply.github.com>
2024-06-21 09:18:52 +0900

    [Gold IV] Title: 부분합, Time: 284 ms, Memory: 24028 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -1,35 +1,32 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
   public static void main(String args[]) throws Exception {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringTokenizer st = new StringTokenizer(br.readLine());
+
     int N = Integer.parseInt(st.nextToken());
     int S = Integer.parseInt(st.nextToken());
-
     int[] arr = new int[N + 1];
+    int total = 0;
+    int sum = 0;
+    int cnt = 0;
+    int min = N;
+
     st = new StringTokenizer(br.readLine());
     for (int i = 1; i <= N; i++) {
-      arr[i] = arr[i - 1] + Integer.parseInt(st.nextToken());
-    }
-
-    int min = N + 1;
-    int a = 1;
-    int b = 1;
-    while (a <= N) {
-      if (arr[a] - arr[b - 1] >= S) {
-        if (a == b) {
-          System.out.println(1);
-          return;
-        }
-        min = Math.min(min, a - b + 1);
-        b++;
-      } else {
-        a++;
+      arr[i] = Integer.parseInt(st.nextToken());
+      total += arr[i];
+      sum += arr[i];
+      cnt++;
+      while (sum >= S) {
+        min = Math.min(min, cnt);
+        cnt--;
+        sum -= arr[i - cnt];
       }
     }
-    System.out.println(min == N + 1 ? 0 : min);
+
+    System.out.println(total < S ? 0 : min);
   }
 }

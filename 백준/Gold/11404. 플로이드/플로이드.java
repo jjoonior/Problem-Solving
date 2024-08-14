5ccf9fc0fcd5544175a3d813cdb0652d10766e81
jjoonior <121348399+jjoonior@users.noreply.github.com>
2024-08-14 16:16:58 +0900

    [Gold IV] Title: 플로이드, Time: 496 ms, Memory: 43832 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.java
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -1,51 +1,42 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        int n = Integer.parseInt(br.readLine());
-        int m = Integer.parseInt(br.readLine());
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int m = Integer.parseInt(br.readLine());
+		int INF = 10000000;
 
-        int[][] graph = new int[n + 1][n + 1];
+		int[][] map = new int[n + 1][n + 1];
 
-        int INF=10000000;
+		for (int i = 1; i <= n; i++) {
+			Arrays.fill(map[i], INF);
+			map[i][i] = 0;
+		}
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                graph[i][j]=INF;
-                if(i==j)
-                    graph[i][j]=0;
-            }
-        }
+		for (int i = 0; i < m; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int s = Integer.parseInt(st.nextToken());
+			int e = Integer.parseInt(st.nextToken());
+			int w = Integer.parseInt(st.nextToken());
 
-        for (int M = 0; M < m; M++) {
-            st = new StringTokenizer(br.readLine());
-            int i=Integer.parseInt(st.nextToken());
-            int j=Integer.parseInt(st.nextToken());
-            int w=Integer.parseInt(st.nextToken());
+			map[s][e] = Math.min(w, map[s][e]);
+		}
 
-            graph[i][j] = Math.min(graph[i][j],w);
-        }
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++) {
+				for (int k = 1; k <= n; k++) {
+					map[j][k] = Math.min(map[j][k], map[j][i] + map[i][k]);
+				}
+			}
+		}
 
-        for (int k = 1; k <= n; k++) {
-            for (int i = 1; i <= n; i++) {
-                for (int j = 1; j <= n; j++) {
-                    graph[i][j]=Math.min(graph[i][j],graph[i][k]+graph[k][j]);
-                }
-            }
-        }
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                if(graph[i][j]==INF)
-                    System.out.print("0 ");
-                else
-                    System.out.print(graph[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++) {
+				System.out.print((map[i][j] = map[i][j] == INF ? 0 : map[i][j]) + " ");
+			}
+			System.out.println();
+		}
+	}
 }

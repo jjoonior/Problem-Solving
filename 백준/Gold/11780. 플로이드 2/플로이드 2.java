d127e0e9c784b449a053b6a1322ff95fa48becf0
jjoonior <121348399+jjoonior@users.noreply.github.com>
2024-08-16 09:34:02 +0900

    [Gold II] Title: 플로이드 2, Time: 532 ms, Memory: 59124 KB -BaekjoonHub

--- 백준/Gold/11780. 플로이드 2/플로이드 2.java
+++ 백준/Gold/11780. 플로이드 2/플로이드 2.java
@@ -2,82 +2,63 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int n, INF = 100_001;
-	static int[][] map;
-	static List<Integer>[][] list;
-	static StringBuilder sb;
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		n = Integer.parseInt(br.readLine());
+		int n = Integer.parseInt(br.readLine());
 		int m = Integer.parseInt(br.readLine());
-		
-		sb = new StringBuilder();
-		map = new int[n+1][n+1];
-		list = new ArrayList[n+1][n+1];
-		for(int i=1; i<=n; i++) {
-			for(int j=1; j<=n; j++) {
+		int INF = 10000001;
+		StringBuilder sb = new StringBuilder();
+
+		int[][] map = new int[n + 1][n + 1];
+		List<Integer>[][] list = new List[n + 1][n + 1];
+
+		for (int i = 1; i <= n; i++) {
+			Arrays.fill(map[i], INF);
+			map[i][i] = 0;
+			for (int j = 1; j <= n; j++) {
 				list[i][j] = new ArrayList<>();
-				if(i!=j) {
-					map[i][j] = INF;
-				}
+//				if (i != j) {
+//					list[i][j].add(i);
+//					list[i][j].add(j);
+//				}
 			}
 		}
-		
-		StringTokenizer st;
-		for(int i=0; i<m; i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
+
+		for (int i = 0; i < m; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int s = Integer.parseInt(st.nextToken());
+			int e = Integer.parseInt(st.nextToken());
 			int w = Integer.parseInt(st.nextToken());
-			map[a][b] = Math.min(map[a][b],w);
+
+			map[s][e] = Math.min(w, map[s][e]);
 		}
-		
-		for(int k=1; k<=n; k++) {
-			for(int i=1; i<=n; i++) {
-				for(int j=1; j<=n; j++) {
-					if(map[i][j] > map[i][k]+map[k][j]) {
-						map[i][j] = map[i][k]+map[k][j];
-						trackingRoute(i, k, j);
+
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++) {
+				for (int k = 1; k <= n; k++) {
+					if (map[j][k] > map[j][i] + map[i][k]) {
+						list[j][k].clear();
+						list[j][k].addAll(list[j][i]);
+						list[j][k].add(i);
+//						list[j][k].remove(list[j][k].size()-1);
+						list[j][k].addAll(list[i][k]);
+						map[j][k] = map[j][i] + map[i][k];
 					}
 				}
 			}
 		}
-		
-		printMap();
-		printRoute();
-		System.out.println(sb.toString());
-		
-	}
 
-	private static void trackingRoute(int i, int k, int j) {
-		list[i][j].clear();
-		for(int route : list[i][k]) {
-			list[i][j].add(route);
-		}
-		list[i][j].add(k);
-		for(int route : list[k][j]) {
-			list[i][j].add(route);
-		}
-	}
-	
-	static void printMap() {
-		for(int i=1; i<=n; i++) {
-			for(int j=1; j<=n; j++) {
-				if(map[i][j] == INF) {
-					sb.append(0+" ");
-					continue;
-				}
-				sb.append(map[i][j]+" ");
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++) {
+				sb.append((map[i][j] = map[i][j] == INF ? 0 : map[i][j]) + " ");
 			}
 			sb.append("\n");
 		}
-	}
-	
-	static void printRoute() {
-		for(int i=1; i<=n; i++) {
-			for(int j=1; j<=n; j++) {
+
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++) {
 				int size = list[i][j].size();
-				if(i==j || map[i][j] == INF) {
+				if(map[i][j] == 0) {
 					sb.append(0+"\n");
 					continue;
 					
@@ -87,7 +68,19 @@ public class Main {
 					sb.append(num+" ");
 				}
 				sb.append(j+"\n");
+				
+				
+//				if (!list[i][j].isEmpty()) {
+//					sb.append(list[i][j].size() + " ");
+//					for (int k = 0; k < list[i][j].size(); k++) {
+//						sb.append(list[i][j].get(k) + " ");
+//					}
+//				} else {
+//					sb.append(0);
+//				}
+//				sb.append("\n");
 			}
 		}
+		System.out.println(sb);
 	}
 }

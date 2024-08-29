2d3979239eea7badcc90b9c994949489ec057f25
허인주 <121348399+jjoonior@users.noreply.github.com>
2024-08-29 09:27:51 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 476 ms, Memory: 49616 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -2,65 +2,64 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-  static int V, E;
-  static boolean[] visited;
-  static List<Edge>[] graph;
+	static int[] p;
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
+		int V = Integer.parseInt(st.nextToken());
+		int E = Integer.parseInt(st.nextToken());
+		p = new int[V+1];
+		
+		for (int i = 1; i <= V; i++) {
+			p[i]=i;
+		}
+		
+		PriorityQueue<Edge> pq = new PriorityQueue<>();
+		
+		for (int i = 0; i < E; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a =Integer.parseInt(st.nextToken());
+			int b =Integer.parseInt(st.nextToken());
+			int c =Integer.parseInt(st.nextToken());
+			pq.add(new Edge(a,b,c));
+		}
 
-  public static void main(String args[]) throws Exception {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st = new StringTokenizer(br.readLine());
-
-    V = Integer.parseInt(st.nextToken());
-    E = Integer.parseInt(st.nextToken());
-    visited = new boolean[V + 1];
-    graph = new List[V + 1];
-    for (int i = 1; i <= V; i++) {
-      graph[i] = new ArrayList<>();
-    }
-
-    for (int i = 0; i < E; i++) {
-      st = new StringTokenizer(br.readLine());
-      int u = Integer.parseInt(st.nextToken());
-      int v = Integer.parseInt(st.nextToken());
-      int w = Integer.parseInt(st.nextToken());
-      graph[u].add(new Edge(v, w));
-      graph[v].add(new Edge(u, w));
-    }
-
-    PriorityQueue<Edge> pq = new PriorityQueue<>();
-    pq.offer(new Edge(1, 0));
-
-    int cost = 0;
-    while (!pq.isEmpty()) {
-      Edge e = pq.poll();
-      if (!visited[e.v]) {
-        visited[e.v] = true;
-        cost += e.w;
-        for (Edge edge : graph[e.v]) {
-          if (!visited[edge.v]) {
-            pq.offer(edge);
-          }
-        }
-      }
-    }
-
-    System.out.println(cost);
-  }
-
+		int sum = 0;
+		while(!pq.isEmpty()) {
+			Edge edge = pq.poll();
+			
+			if(findSet(edge.u) != findSet(edge.v)) {
+				sum+= edge.w;
+				union(edge.u, edge.v);
+			}
+		}
+	
+		System.out.println(sum);
+	}
+	
+	static int findSet(int a) {
+		return p[a] == a ? a : (p[a] = findSet(p[a]));
+	}
+	
+	static void union(int a, int b) {
+		p[findSet(a)] =  findSet(b);
+	}
 }
 
-class Edge implements Comparable<Edge> {
-  int v;
-  int w;
-
-  Edge(int v, int w) {
-    this.v = v;
-    this.w = w;
-  }
-
-  @Override
-  public int compareTo(Edge o) {
-    return this.w - o.w;
-  }
+class Edge implements Comparable<Edge>{
+	int u;
+	int v;
+	int w;
+	
+	public Edge(int u, int v, int w) {
+		this.u = u;
+		this.v = v;
+		this.w = w;
+	}
+	
+	public int compareTo(Edge o) {
+		return this.w - o.w;
+	}
 }

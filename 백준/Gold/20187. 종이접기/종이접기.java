c250d057196ac085a4ea58fbeb65b3b36055dad8
허인주 <121348399+jjoonior@users.noreply.github.com>
2024-09-02 17:35:15 +0900

    [Gold III] Title: 종이접기, Time: 188 ms, Memory: 24312 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/20187. 종이접기/종이접기.java
+++ 백준/Gold/20187. 종이접기/종이접기.java
@@ -8,7 +8,7 @@ public class Main {
 		int k = Integer.parseInt(br.readLine());
 
 		int[][] point = new int[4][2];
-		point[1][1] = point[2][0] = point[3][0] = point[3][1] = 2 * k;
+		point[1][1] = point[2][0] = point[3][0] = point[3][1] = (int) Math.pow(2, k);
 
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < 2 * k; i++) {
@@ -34,11 +34,12 @@ public class Main {
 		}
 
 		int h = Integer.parseInt(br.readLine());
+		int pow = (int) Math.pow(2, k);
 
-		int[][] map = new int[(int) Math.pow(2, k)][(int) Math.pow(2, k)];
-		boolean[][] visited = new boolean[(int) Math.pow(2, k)][(int) Math.pow(2, k)];
-
+		int[][] map = new int[pow][pow];
+		boolean[][] visited = new boolean[pow][pow];
 		Queue<int[]> q = new LinkedList<>();
+
 		visited[point[0][0]][point[0][1]] = true;
 		map[point[0][0]][point[0][1]] = h;
 		q.offer(new int[] { point[0][0], point[0][1], h });
@@ -49,37 +50,27 @@ public class Main {
 			int[] dy = { 1, -1, 0, 0 };
 			int[] dx = { 0, 0, 1, -1 };
 
-			for (int i = 0; i < 2; i++) {
+			for (int i = 0; i < 4; i++) {
 				int y = e[0] + dy[i];
 				int x = e[1] + dx[i];
 
-				if (y < 0 || y >= (int) Math.pow(2, k) || x < 0 || x >= (int) Math.pow(2, k) || visited[y][x])
+				if (y < 0 || y >= pow || x < 0 || x >= pow || visited[y][x])
 					continue;
 
 				visited[y][x] = true;
-				map[y][x] = (e[2] + 2) % 4;
+				map[y][x] = i < 2 ? (e[2] + 2) % 4 : (e[2] == 1 || e[2] == 3) ? e[2] - 1 : e[2] + 1;
 				q.offer(new int[] { y, x, map[y][x] });
 			}
-
-			for (int i = 2; i < 4; i++) {
-				int y = e[0] + dy[i];
-				int x = e[1] + dx[i];
-
-				if (y < 0 || y >= (int) Math.pow(2, k) || x < 0 || x >= (int) Math.pow(2, k) || visited[y][x])
-					continue;
-
-				visited[y][x] = true;
-				map[y][x] = e[2] == 1 || e[2] == 3 ? e[2] - 1 : e[2] + 1;
-				q.offer(new int[] { y, x, map[y][x] });
-			}
-
 		}
 
-		for (int i = 0; i < (int) Math.pow(2, k); i++) {
-			for (int j = 0; j < (int) Math.pow(2, k); j++) {
-				System.out.print(map[i][j] + " ");
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < pow; i++) {
+			for (int j = 0; j < pow; j++) {
+				sb.append(map[i][j]).append(" ");
 			}
-			System.out.println();
+			sb.append("\n");
 		}
+
+		System.out.println(sb);
 	}
 }
